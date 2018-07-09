126fd51babcbe30d09e67be462a467a6ff0b7d5e
Pranay Neelagiri <pneeldam@gmail.com>
2018-07-08 23:29:49 -0400

    Begin AnnouncementPageView

---
+++ SakaiClientiOS/UIElements/AnnouncementPageView.swift
@@ -0,0 +1,32 @@
+//
+//  AnnouncementView.swift
+//  SakaiClientiOS
+//
+//  Created by Pranay Neelagiri on 7/8/18.
+//
+
+import UIKit
+
+class AnnouncementPageView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setup() {
+        
+    }
+    
+    func addViews() {
+        
+    }
+    
+    func setConstraints() {
+        
+    }
+}
