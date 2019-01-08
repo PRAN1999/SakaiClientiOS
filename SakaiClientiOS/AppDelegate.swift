539cbba9b036d92a7c2719739977b14dc10f598e
Pranay Neelagiri <pneeldam@gmail.com>
2019-01-08 01:00:32 -0500

    Document directory flushing. Remove all downloaded documents on app entering background

--- SakaiClientiOS/AppDelegate.swift
+++ SakaiClientiOS/AppDelegate.swift
@@ -29,7 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-
+        FileManager.default.clearDocumentsDirectory()
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -48,8 +48,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if
-        // appropriate. See also applicationDidEnterBackground:.
+
     }
     
     func application(_ application: UIApplication,
